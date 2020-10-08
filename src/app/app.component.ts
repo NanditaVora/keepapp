5df5904a1462aff128ca406e1d2647e5b54ec3bf
NanditaVora <nanditavora@yahoo.co.in>
2020-10-08 11:39:41 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+class Note{
+  title:string='';
+  text:string='';
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'keepapp';
+  textColor: string = "white";
+  errorMessage: string = "";
+
+  notes: Array<Note> = [];
+
+  note : Note = new Note();
+
+  validate(event){
+    console.log(event);
+    if(event.target.value.length < 6){
+      this.textColor = "red"
+      this.errorMessage = 'Title should be of minimum 6 characters long';
+    }
+    else{
+      this.errorMessage=''
+      this.textColor = "lightgreen"
+    }
+  }
+
+  addNote(){
+    this.notes.push(this.note);
+    console.log(this.notes);
+    console.log('Note Added')
+    this.note = new Note();
+  }
+
+  onChangeTitle(event){
+    this.note.title = event.target.value;
+  }
+  
+  onChangeText(event){
+    this.note.text = event.target.value;
+  }
+
 }
